bbd7543fc4316028cef77d9e76e191baf7a6b5a8
douglasd11 <dguerreroe@unicartagena.edu.co>
2023-07-22 04:17:30 -0500

    test HashRouter

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -7,19 +7,20 @@ import AgregarProducto from './pages/AgregarProducto';
 import EditarProducto from './pages/EditarProducto';
 
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+//import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Switch, Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path='/' element={<Landing/>}></Route>
         <Route path='/listaproductos' element={<ListaProductos></ListaProductos>}></Route>
         <Route path='/agregarproducto' element={<AgregarProducto></AgregarProducto>}></Route>
         <Route path='/editarproducto/:codigoproducto' element={<EditarProducto></EditarProducto>}></Route>
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
